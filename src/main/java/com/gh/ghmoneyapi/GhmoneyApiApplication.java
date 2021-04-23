9a487acaf9e9b70a79c93872bd2e5c59611814e2
DevGuima <glecioheybel@hotmail.com>
2021-04-22 21:04:28 -0300

    Commit Inicial

---
+++ src/main/java/com/gh/ghmoneyapi/GhmoneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.gh.ghmoneyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhmoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhmoneyApiApplication.class, args);
+	}
+
+}
