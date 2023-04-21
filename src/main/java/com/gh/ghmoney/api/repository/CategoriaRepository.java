94d8ca21d942a3f01e1fa868f7aafed65d8c422e
DevGuima <glecioheybel@hotmail.com>
2023-04-21 12:47:20 -0300

    Commit com criacao das camadas model, resource e repository e alguns endpoints

---
+++ src/main/java/com/gh/ghmoney/api/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.gh.ghmoney.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gh.ghmoney.api.model.Categoria;
+
+public interface CategoriaRepository  extends JpaRepository<Categoria, Long>{
+
+}
---
+++ src/main/java/com/gh/ghmoney/api/resource/CategoriaResource.java
@@ -0,0 +1,52 @@
+package com.gh.ghmoney.api.resource;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.gh.ghmoney.api.model.Categoria;
+import com.gh.ghmoney.api.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+	
+	@Autowired	
+	private CategoriaRepository categoriaRepository;
+
+	@GetMapping
+	public List<Categoria> listarCategorias(){
+		return categoriaRepository.findAll();
+		
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> criarCategoria(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+
+		return ResponseEntity.created(uri).body(categoriaSalva);
+
+	}
+	
+	@GetMapping("/{codigo}")
+	public Categoria buscarPorCodigo(@PathVariable Long codigo) {
+		 return categoriaRepository.findById(codigo).orElse(null);
+		
+	}
+}
