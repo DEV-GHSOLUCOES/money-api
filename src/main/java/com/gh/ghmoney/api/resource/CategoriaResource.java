73f1ecf5b5e7eab154642cef2ae5e66b22205e7a
DevGuima <glecioheybel@hotmail.com>
2023-04-23 11:44:06 -0300

    commit com criacao do handler de excecoes

--- src/main/java/com/gh/ghmoney/api/resource/CategoriaResource.java
+++ src/main/java/com/gh/ghmoney/api/resource/CategoriaResource.java
@@ -2,18 +2,17 @@ package com.gh.ghmoney.api.resource;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -23,16 +22,16 @@ import com.gh.ghmoney.api.repository.CategoriaRepository;
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
-	
-	@Autowired	
+
+	@Autowired
 	private CategoriaRepository categoriaRepository;
 
 	@GetMapping
-	public List<Categoria> listarCategorias(){
+	public List<Categoria> listarCategorias() {
 		return categoriaRepository.findAll();
-		
+
 	}
-	
+
 	@PostMapping
 	public ResponseEntity<Categoria> criarCategoria(@RequestBody Categoria categoria, HttpServletResponse response) {
 		Categoria categoriaSalva = categoriaRepository.save(categoria);
@@ -43,10 +42,11 @@ public class CategoriaResource {
 		return ResponseEntity.created(uri).body(categoriaSalva);
 
 	}
-	
+
 	@GetMapping("/{codigo}")
-	public Categoria buscarPorCodigo(@PathVariable Long codigo) {
-		 return categoriaRepository.findById(codigo).orElse(null);
-		
+	public ResponseEntity<Categoria> buscarPorCodigo(@PathVariable Long codigo) {
+		Optional<Categoria> categoria = categoriaRepository.findById(codigo);
+		return categoria.isPresent() ? ResponseEntity.ok(categoria.get()) : ResponseEntity.notFound().build();
+
 	}
 }
