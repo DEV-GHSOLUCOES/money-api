063543c263d529c3c0e5e92e57ce336b7f4a259c
DevGuima <glecioheybel@hotmail.com>
2023-05-06 21:37:22 -0300

    commit com criacao de eventos para adicionar header location na criacao dos recursos

---
+++ src/main/java/com/gh/ghmoney/api/event/RecursoCriadoEvent.java
@@ -0,0 +1,30 @@
+package com.gh.ghmoney.api.event;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.ApplicationEvent;
+
+public class RecursoCriadoEvent extends ApplicationEvent {
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Long codigo;
+	private HttpServletResponse response;
+
+	public RecursoCriadoEvent(Object source, HttpServletResponse response, Long codigo) {
+		super(source);
+		this.codigo =  codigo;
+		this.response =  response;
+		
+	}
+	
+	public Long getCodigo() {
+		return codigo;
+	}
+	
+	public HttpServletResponse getResponse() {
+		return response;
+	}
+
+}
---
+++ src/main/java/com/gh/ghmoney/api/event/listener/RecursoCriadoListener.java
@@ -0,0 +1,33 @@
+package com.gh.ghmoney.api.event.listener;
+
+import java.net.URI;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.gh.ghmoney.api.event.RecursoCriadoEvent;
+
+
+@Component
+public class RecursoCriadoListener implements ApplicationListener<RecursoCriadoEvent>{
+
+	@Override
+	public void onApplicationEvent(RecursoCriadoEvent event) {
+		
+		HttpServletResponse response =  event.getResponse();
+		Long codigo =  event.getCodigo();
+		
+		adcionarHeaderLocation(response, codigo);
+		
+	}
+
+	private void adcionarHeaderLocation(HttpServletResponse response, Long codigo) {
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(codigo).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+	}
+
+}
