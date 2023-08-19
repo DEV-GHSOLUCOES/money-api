d95e09d5a883a4430c8452b1bc9078f9b457ba60
DEV-GHSOLUCOES <glecioheybel@hotmail.com>
2023-08-19 17:08:09 -0300

    Commit com config do JWT

--- src/main/java/com/gh/ghmoney/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/gh/ghmoney/api/config/AuthorizationServerConfig.java
@@ -9,8 +9,11 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -33,17 +36,22 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints
 		.tokenStore(tokenStore())
+		.accessTokenConverter(accessTokenConverter())
 		.authenticationManager(authenticationManager);
 		
 	}
 	
 	@Bean
-	public TokenStore tokenStore() {
-		return new InMemoryTokenStore();
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+		accessTokenConverter.setSigningKey("3032885ba9cd6621bcc4e7d6b6c35c2b");
+		return accessTokenConverter;
 	}
 	
-	@Override
-	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-	    security.checkTokenAccess("permitAll()");
+	@Bean
+	public TokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
 	}
+	
+	
 }
